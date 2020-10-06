d4088f4a7227384617f2149dc4d5c2d69cec6e0d
Brian Gulotta <bgulotta@Brians-iMac.local>
2020-10-06 13:41:33 -0500

    Start of scrolling new level data.

--- viewport.asm
+++ viewport.asm
@@ -1,9 +1,42 @@
 UPDATE_VIEWPORT:
+    LDA Camera + ACTOR_DATA::Movement
     BEQ UPDATE_VIEWPORT_EXIT   ; If the camera isn't moving then the viewport is where it needs to be
+    JSR SAVE_VIEWPORT_POSITION
     JSR SET_VIEWPORT_BEGIN
     JSR SET_VIEWPORT_END
+    JSR UPDATE_LEVEL
     JSR UPDATE_SCROLL
 UPDATE_VIEWPORT_EXIT:
+    RTS
+
+UPDATE_LEVEL:
+    LDA Temp 
+    EOR ViewPort + ViewPort::Begin
+    AND #$20
+    BEQ UPDATE_LEVEL_EXIT
+UPDATE_LEVEL_DATA:
+    LDA Camera + Actor::Attributes
+    AND #JOYPAD::Right
+    BEQ UPDATE_LEVEL_DATA_LEFT
+UPDATE_LEVEL_DATA_RIGHT:
+    LDA #.LOBYTE(ViewPort + ViewPort::End)
+    STA COORDINATES_PTR
+    LDA #.HIBYTE(ViewPort + ViewPort::End)
+    STA COORDINATES_PTR + 1
+    JSR UPDATE_LEVEL_SCROLL
+    JMP UPDATE_LEVEL_EXIT
+UPDATE_LEVEL_DATA_LEFT:
+    LDA #.LOBYTE(ViewPort + ViewPort::Begin)
+    STA COORDINATES_PTR
+    LDA #.HIBYTE(ViewPort + ViewPort::Begin)
+    STA COORDINATES_PTR + 1
+    JSR UPDATE_LEVEL_SCROLL
+UPDATE_LEVEL_EXIT: 
+    RTS 
+
+SAVE_VIEWPORT_POSITION:
+    LDA ViewPort + ViewPort::Begin
+    STA Temp
     RTS
 
 UPDATE_SCROLL:
