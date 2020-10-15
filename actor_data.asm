5a6a3cf69f4b339eb867a80006c972434e0f7d98
Brian Gulotta <bgulotta@Brians-iMac.local>
2020-10-15 10:11:36 -0500

    Added placeholder graphics. Ready to build out HUD

--- actor_data.asm
+++ actor_data.asm
@@ -11,6 +11,6 @@ CameraMeta:
 Tiles:
     ; Num tiles, [YOffset, Tile Index, Attributes, XOffset] 
 PlayerTiles:
-    .BYTE $04, $00, $01, $01, $00, $00, $01, $40, $06, $06, $01, $80, $00, $06, $01, $C0, $06     
+    .BYTE $04, $00, $DA, $00, $00, $00, $DB, $00, $08, $08, $D5, $00, $00, $08, $D6, $00, $08     
 CameraTiles:
-    .BYTE $01, $00, $01, $00, $00     
+    .BYTE $01, $00, $2D, $00, $00     
